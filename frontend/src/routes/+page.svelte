0009fcd50b1cbacd7db68b845ecfdb880e0ccb2f
Hannah-Hoover <hd3611@wayne.edu>
2024-01-09 16:54:43 -0500

    Update +page.svelte
    made selection a list on homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,15 @@
 <h1>Hello User, Please select account type</h1>
-<a href="/login">Caregiver</a>
+
+<form action="/login">
+    <div>
+   <input type="radio" id="caregiver" name="category" value="caregiver">
+   <label for="caregiver">Caregiver</label>
+    </div>
+   <div>
+   <input type="radio" id="patient" name="category" value="patient">
+   <label for="patient">Patient</label>
+</div>
+<div>
+    <button>Submit</button>
+</div>
+</form>
